c457ca5ee18537d534ad68a35975117dd9a42604
fire0607 <stu00634@gmail.com>
2025-05-25 00:14:41 +0800

    v1.0.1 - 部署設定

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>台南女子監獄聚餐意向調查</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <script type="module" src="{{ asset('build/assets/app.js') }}"></script>
 </head>
 
 <body class="bg-gray-300 min-h-screen">
